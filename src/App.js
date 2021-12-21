93383417e9d6b4c98d96517b2a3e6cccf293bc8d
SnowBit <dtshah24022006@gmail.com>
2021-12-21 10:11:42 +0530

    Completed 🎉

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Input from './Input'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Redirect from './Redirect';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Input />} />
+          <Route path="/:slug" element={<Redirect />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
